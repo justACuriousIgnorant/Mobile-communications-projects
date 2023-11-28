0c3d11f05b95951164384795b95d00b7ee57097d
justACuriousIgnorant <new.log.error@gmail.com>
2023-11-28 14:34:10 +0100

    added plotting methods

--- Progetto 1/exercise 1/PlotterClass.py
+++ Progetto 1/exercise 1/PlotterClass.py
@@ -2,11 +2,12 @@ import math
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 from pylab import cm
-
+import numpy as np
 class PlotterClass:
 
-    def __init__(self, data):
+    def __init__(self, data, params):
         self.probs = data
+        self.params = params
 
     def __truncate(self, number, digits) -> float:
         # Improve accuracy with floating point operations, to avoid truncate(16.4, 2) = 16.39 or truncate(-1.13, 2) = -1.12
@@ -16,22 +17,46 @@ class PlotterClass:
         stepper = 10.0 ** digits
         return math.trunc(stepper * number) / stepper
 
-    def plot_sigma(self):
+    def generate_slider(self, label, valmin, valmax, valinit, valstep, axes_list):
+
+        f = plt.axes(axes_list)
+        slider = Slider(f, label, valmin, valmax, valinit=valinit, valstep=valstep)
+        return slider
+
+
+    def gen_data_sigma(self,sigmas):
+        plots = []
+        for sigma in sigmas:
+            x = [x[0] for x in self.probs if x[1] == sigma and x[3]==self.params['f']]  # distance
+            y = [x[2] for x in self.probs if x[1] == sigma and x[3]==self.params['f']]  # prob
+            plots.append((x, y))
+        return plots
+    def gen_data_distance(self,distances):
+        plots = []
+        for dist in distances:
+            x = [x[1] for x in self.probs if x[0] == dist and x[3]==self.params['f']]  # distance
+            y = [x[2] for x in self.probs if x[0] == dist and x[3]==self.params['f']]  # prob
+            plots.append((x, y))
+        return plots
+
+    #plot_sigma() effettua il print del plot con:
+    # - asse x la distanza
+    # - asse y la probabilit
+    # - slider per il valore di sigma
+    def plot_slider_sigma(self):
         # Create a subplot
         fig, ax = plt.subplots()
         plt.subplots_adjust(bottom=0.35)
         plt.ylim((0, 1))
-        sv = 0.1  # define the starting value of sigma
+        sv = 6  # define the starting value of sigma
         x = [x[0] for x in self.probs if x[1] == sv]
         y = [x[2] for x in self.probs if x[1] == sv]
         l, = plt.plot(x, y)
 
-        sigma = plt.axes([0.25, 0.2, 0.65, 0.03])
-
-        sigma_slider = Slider(sigma, 'Sigma', 0, 12.01, sv, 0.01)
+        slider = self.generate_slider("Sigma: ", 0,12.01,sv,0.01, [0.25, 0.2, 0.65, 0.03])
 
         def update(val):
-            bound = self.__truncate(sigma_slider.val, 2)
+            bound = self.__truncate(slider.val, 2)
             # probabilità con varianza fissa e distanza variabile
             x = [x[0] for x in self.probs if x[1] == bound]
             y = [x[2] for x in self.probs if x[1] == bound]
@@ -40,12 +65,32 @@ class PlotterClass:
             print(y)
 
         # Call update function when slider value is changed
-        sigma_slider.on_changed(update)
+        slider.on_changed(update)
 
         # display graph
         plt.show()
 
-    def plot_distance(self):
+
+    def plot_sigmas(self):
+        # Create a subplot
+        fig, ax = plt.subplots()
+        plt.subplots_adjust(bottom=0.35)
+        plt.xlim((1, 10))
+        sv = 0.1  # define the starting value of sigma
+        sigmas = [1, 3, 5, 8, 12]
+        colors = ["#2f2424", "#8ab09e", "#ffbdbd", "#d94949", "#57885a"]
+        plots_data = self.gen_data_sigma(sigmas)
+        for i in range(len(sigmas)):
+            ax.plot(plots_data[i][0], plots_data[i][1], label="s = "+str(sigmas[i]), color=colors[i])
+        ax.legend()
+        plt.xlabel("Distance (km) ")
+        plt.ylabel("Probability")
+        plt.title("Probability(distance)")
+        # display graph
+        plt.show()
+
+
+    def plot_slider_distance(self):
         # Create a subplot
         fig, ax = plt.subplots()
         plt.subplots_adjust(bottom=0.35)
@@ -55,11 +100,10 @@ class PlotterClass:
         y = [x[2] for x in self.probs if x[0] == sd]
         l, = plt.plot(x, y)
 
-        distance = plt.axes([0.25, 0.2, 0.65, 0.03])
-        distance_slider = Slider(distance, 'Distance (km)', 0, 10.1, sd, 0.1)
+        slider = self.generate_slider('Distance (km)', 0, 10.1, sd, 0.1,[0.25, 0.2, 0.65, 0.03])
 
         def update(val):
-            bound = self.__truncate(distance_slider.val, 1)
+            bound = self.__truncate(slider.val, 1)
             # probabilità con varianza fissa e distanza variabile
             x = [x[1] for x in self.probs if x[0] == bound]
             y = [x[2] for x in self.probs if x[0] == bound]
@@ -68,11 +112,27 @@ class PlotterClass:
             print(y)
 
         # Call update function when slider value is changed
-        distance_slider.on_changed(update)
+        slider.on_changed(update)
 
         # display graph
         plt.show()
 
+    def plot_distances(self):
+        # Create a subplot
+        fig, ax = plt.subplots()
+        plt.subplots_adjust(bottom=0.35)
+        plt.xlim((0, 12))
+        distances = [1, 3, 5, 7, 10]
+        colors = ["#2f2424", "#8ab09e", "#ffbdbd", "#d94949", "#57885a"]
+        plots_data = self.gen_data_distance(distances)
+        for i in range(len(distances)):
+            ax.plot(plots_data[i][0], plots_data[i][1], label="d = " + str(distances[i]), color=colors[i])
+        ax.legend()
+        plt.xlabel("Sigmas  ")
+        plt.ylabel("Probability")
+        plt.title("Probability(sigma)")
+        # display graph
+        plt.show()
 
     def plot_loss_distance(self):
         # Create a subplot
@@ -84,11 +144,9 @@ class PlotterClass:
         y = [x[2] for x in self.probs if x[0] == sd]
         l, = plt.plot(x, y)
 
-        distance = plt.axes([0.25, 0.2, 0.65, 0.03])
-        distance_slider = Slider(distance, 'Distance (km)', 0, 10.1, sd, 0.1)
-
+        slider = self.generate_slider('Distance (km)', 0, 10.1, sd, 0.1,[0.25, 0.2, 0.65, 0.03])
         def update(val):
-            bound = self.__truncate(distance_slider.val, 1)
+            bound = self.__truncate(slider.val, 1)
             # probabilità con varianza fissa e distanza variabile
             x = [x[1] for x in self.probs if x[0] == bound]
             y = [x[2] for x in self.probs if x[0] == bound]
@@ -97,11 +155,25 @@ class PlotterClass:
             print(y)
 
         # Call update function when slider value is changed
-        distance_slider.on_changed(update)
+        slider.on_changed(update)
 
         # display graph
         plt.show()
 
+    def gen_data_freq_sigma(self, bound):
+        plots = []
+        for freq in [1700, 1800, 1900, 2000]:
+            print(self.probs)
+            x = [x[0] for x in self.probs if ((x[1] == bound) and (x[3] == freq))]  # distance
+            y = [x[2] for x in self.probs if ((x[1] == bound) and (x[3] == freq))]  # prob
+            plots.append((x, y))
+        return plots
+
+    def set_data_plots(self, plots_data, plots):
+        for plot, data in plots, plots_data:
+
+            plot.set_xdata(data[0])
+            plot.set_ydata(data[1])
 
     def plot_loss_sigma(self):
         # Create a subplot
@@ -109,57 +181,15 @@ class PlotterClass:
         plt.subplots_adjust(bottom=0.35)
         plt.xlim((1, 10))
         sv = 0.1  # define the starting value of sigma
-        #I select sigma and
 
-        x1 = [x[0] for x in self.probs if x[1] == sv and x[3] == 1700]    #distance
-        y1 = [x[2] for x in self.probs if x[1] == sv and x[3] == 1700]    #sigma
+        plots_data = self.gen_data_freq_sigma(sv)
 
-        x2 = [x[0] for x in self.probs if x[1] == sv and x[3] == 1800]  # distance
-        y2 = [x[2] for x in self.probs if x[1] == sv and x[3] == 1800]  # sigma
-
-        x3 = [x[0] for x in self.probs if x[1] == sv and x[3] == 1900]  # distance
-        y3 = [x[2] for x in self.probs if x[1] == sv and x[3] == 1900]  # sigma
-
-        x4 = [x[0] for x in self.probs if x[1] == sv and x[3] == 2000]  # distance
-        y4 = [x[2] for x in self.probs if x[1] == sv and x[3] == 2000]  # sigma
-
-        l1, = ax.plot(x1, y1,label="f=1700",color="#22092C")
-        l2, = ax.plot(x2, y2,label="f=1800",color="#872341")
-        l3, = ax.plot(x3, y3,label="f=1900",color="#BE3144")
-        l4, = ax.plot(x4, y4,label="f=2000",color="#F05941")
+        ax.plot(plots_data[0][0], plots_data[0][1],label="f=1700",color="#22092C")
+        ax.plot(plots_data[1][0], plots_data[1][1],label="f=1800",color="#872341")
+        ax.plot(plots_data[2][0], plots_data[2][1],label="f=1900",color="#BE3144")
+        ax.plot(plots_data[3][0], plots_data[3][1],label="f=2000",color="#F05941")
 
         ax.legend()
-
-        sigma = plt.axes([0.25, 0.2, 0.65, 0.03])
-        sigma_slider = Slider(sigma, 'Sigma:', 0, 12.01,  valinit=0.1, valstep=0.01)
-
-        def update(val):
-            bound = self.__truncate(sigma_slider.val, 2)
-            # probabilità con varianza fissa e distanza variabile
-            x1 = [x[0] for x in self.probs if x[1] == bound and x[3] == 1700]
-            y1 = [x[2] for x in self.probs if x[1] == bound and x[3] == 1700]
-
-            x2 = [x[0] for x in self.probs if x[1] == bound and x[3] == 1800]  # distance
-            y2 = [x[2] for x in self.probs if x[1] == bound and x[3] == 1800]  # sigma
-
-            x3 = [x[0] for x in self.probs if x[1] == bound and x[3] == 1900]  # distance
-            y3 = [x[2] for x in self.probs if x[1] == bound and x[3] == 1900]  # sigma
-
-            x4 = [x[0] for x in self.probs if x[1] == bound and x[3] == 2000]  # distance
-            y4 = [x[2] for x in self.probs if x[1] == bound and x[3] == 2000]  # sigma
-
-            l1.set_xdata(x1)
-            l1.set_ydata(y1)
-            l2.set_xdata(x2)
-            l2.set_ydata(y2)
-            l3.set_xdata(x3)
-            l3.set_ydata(y3)
-            l4.set_xdata(x4)
-            l4.set_ydata(y4)
-            print(y1)
-
-        # Call update function when slider value is changed
-        sigma_slider.on_changed(update)
 
         # display graph
         plt.show()
@@ -210,31 +240,57 @@ class PlotterClass:
         plt.show()
 
 
+    def gen_data_freq_barplot(self, bound_dv, bound_sv):
+        plots = []
+        for freq in [1700, 1800, 1900, 2000]:
+            prob = [x[2] for x in self.probs if (x[0] == bound_dv and x[1] == bound_sv and x[3] == freq)]  # distance
+            plots.append(prob)
+
+        plots = [x[0] for x in plots]
+        return plots
+
+    def set_data_plots_barplot(self, plots_data):
+        categories = ['Probability for 1700MHz', 'Probability for 1800MHz', 'Probability for 1900MHz',
+                      'Probability for 2000MHz']
+
+        bar_width = 0.2
+        r = np.arange(len(categories))
+
+        data = plots_data
+
+        plt.xticks([r + bar_width * 1.5 for r in range(len(categories))],categories)  # Imposta le etichette al centro dei gruppi
+
+        plt.bar(r, data, width=bar_width, color='#22092C')
+
+
     def bar_plot_prob(self):
+        categories = ['Probability for 1700MHz', 'Probability for 1800MHz', 'Probability for 1900MHz', 'Probability for 2000MHz']
+
         fig, ax = plt.subplots()
         plt.subplots_adjust(left=0.25, bottom=0.25)
-        sv = 0.01
-        sd = 5.0
-        values = [x[2] for x in self.probs if (x[0] == sd and x[1] == sv)]
-        bar = ax.bar(['probability'], values, label='Probability', color='red')
-        ax.set_ylabel('Probability')
-        ax.set_title('Probability of link')
+        sv = 2.0
+        dv = 5.0
+
+        bar_width = 0.2
+        r = np.arange(len(categories))
+        data = self.gen_data_freq_barplot(dv,sv)
+        print(data)
+        bar = ax.bar(r, data, width=bar_width, edgecolor='#22092C')
+
         plt.ylim((0, 1))
-        sigma = plt.axes([0.25, 0.01, 0.65, 0.03])
-        sigma_slider = Slider(sigma, 'Sigma', 0, 12.01, valinit=0.1, valstep=0.01)
-        distance = plt.axes([0.25, 0.05, 0.65, 0.03])
-        distance_slider = Slider(distance, 'Distance', 1, 10.1, valinit=5, valstep=0.1)
-        bar[0].set_height(values[0])
+        plt.xlabel('frequencies')
+        plt.ylabel('Probability')
+        plt.title('Probability of link')
+        plt.legend()
+        sigma_slider = self.generate_slider("Sigma: ", 0.01,12.01,6, 0.01,[0.25, 0.01, 0.65, 0.03])
+        distance_slider = self.generate_slider("Distance: ",1,10.1,5,0.1,[0.25, 0.05, 0.65, 0.03])
+
         def update(val):
             sigma_bound = self.__truncate(sigma_slider.val, 2)
             distance_bound = self.__truncate(distance_slider.val, 1)
             # probabilità con varianza fissa e distanza variabile
-            values = [x[2] for x in self.probs if (x[0] == distance_bound and x[1] == sigma_bound)]
-            bar[0].set_height(values[0])
-            ax.set_ylabel('Probability')
-            ax.set_xlabel(str(values[0]*100)+"%")
-            ax.set_title('Probability of link')
-            print(values)
+            data = self.gen_data_freq_barplot(distance_bound, sigma_bound)
+            self.set_data_plots_barplot(data)
 
         # Call update function when slider value is changed
         sigma_slider.on_changed(update)
--- Progetto 1/exercise 1/main.py
+++ Progetto 1/exercise 1/main.py
@@ -2,84 +2,37 @@ import math
 import Model
 import numpy as np
 import PlotterClass
+import Simulator
 
-#parameters
-hr = 3      #meters
-ht = 40     #meters
-pr = -96    #dBm
-pt = 56     #dBm
+parameters={
+    'hr':3,     #meters
+    'ht':40,    #meters
+    'pr':-96,   #dBm
+    'pt':56,     #dBm
+    'distance_range': [1, 10.1, 0.1],       #km
+    'sigmas_range': [0, 12.01, 0.01],
+    'frequences': [1700, 1800, 1900, 2000],  #MHz
+    'f':1700
 
-# truncate(number,digits) is a simple function to truncate the numbers when they exceed the number of digits indicated,
-# none cares about it
-def truncate(number, digits) -> float:
-    # Improve accuracy with floating point operations, to avoid truncate(16.4, 2) = 16.39 or truncate(-1.13, 2) = -1.12
-    nbDecimals = len(str(number).split('.')[1])
-    if nbDecimals <= digits:
-        return number
-    stepper = 10.0 ** digits
-    return math.trunc(stepper * number) / stepper
-
-# calculate_prob(L,sigma) that calculates and return the final probability,
-# having the Loss of the Model and the value of sigma
-def calculate_prob(L, sigma):
-    Lmax = pt-pr #dB
-    print("L:", L, ", Lmax: ", Lmax)
-    argument = (Lmax - L) / (math.sqrt(2) * sigma)
-    prob = 0.5 * (1 + math.erf(argument)) #undefined situation when sigma=0
-    return prob
-
-# calculate_couples(dist,sig) calculates the cartesian product between the possible distances
-# and sigmas.
-# calculate_couples require as input:
-# - a triple representing [min_val, max_val, step] for distance
-# - a triple representing [min_val, max_val, step] for sigma
-# the output is a tuple of couples like [(dist1, sig1), (dist1, sig2), ..., (dist2,sig1),(dist2,sig2),...]
-def calculate_couples(dist, sig, freq):
-    distances = [truncate(x,1) for x in np.arange(dist[0], dist[1], dist[2])]
-    sigmas = [truncate(x,2) for x in np.arange(sig[0], sig[1], sig[2])]
-
-    #I remove y=0 because when sigma=0 the formula to calculate the probability is not valid
-    couples = [(x, y, z) for x in distances for y in sigmas for z in freq if y!=0 ]
-    return couples
-
-# calculate_all_prob(couples) calculate the probabilities for each of the couple of the input
-# input:    a tuple of couples, where each couple has the form [dist, sigma]
-# output:   a tuple of triples, where each triple has the form [dist,sigma,prob]
-#           a tuple of triples, where each triple has the form [dist,sigma,loss]
-def calculate_all_prob(couples):
-    probs = []
-    losses = []
-    #couple[0] = distanza
-    #couple[1] = sigma
-    #couple[2] = frequence
-    for couple in couples:
-        L = model.lossdB(couple[2], couple[0])
-        probs.append([couple[0],couple[1],calculate_prob(L, couple[1]), couple[2]])
-        losses.append([couple[0], couple[1], L, couple[2]])
-    return probs,losses
-
+}
 
 if __name__ == '__main__':
-    #first instantiation of the class COST231Model
-    model = Model.COST231Model(hr = hr, ht = ht)
+    simulator = Simulator.Simulator(parameters)
 
-    distance_range = [1,10.1,0.1]
-    sigmas_range = [0,12.01,0.01]
-    frequences = [1700,1800,1900,2000]
-    #I calculate all the possible couples (dist,sigma)
-    couples = calculate_couples(distance_range, sigmas_range,frequences)
     #I calculate all the possible probabilities and losses for each couple
-    probs, losses = calculate_all_prob(couples)
+    probs, losses = simulator.calculate_all_prob()
 
     # I istantiate the class PlotterClass to plot the probabilities
-    plotter_prob = PlotterClass.PlotterClass(probs)
+    plotter_prob = PlotterClass.PlotterClass(probs, parameters)
     # I istantiate the class PlotterClass to plot the losses
-    plotter_loss = PlotterClass.PlotterClass(losses)
+    plotter_loss = PlotterClass.PlotterClass(losses, parameters)
 
     #plotter_prob.plot_distance()
     #plotter_prob.plot_sigma()
-    plotter_loss.plot_loss_sigma()
+    #plotter_loss.plot_loss_sigma()
     #plotter_loss.plot_loss_distance()
     #plotter_prob.plot3D_prob()
     #plotter_loss.plot3D_loss()
     #plotter_prob.bar_plot_prob()
+    plotter_prob.plot_sigmas()
+    plotter_prob.plot_distances()
